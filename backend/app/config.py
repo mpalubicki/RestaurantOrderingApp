70a2df6940b7f11ad8d9c5f4d9f99c7dbb3f23a8
mpalubicki <91080320+mpalubicki@users.noreply.github.com>
2026-01-11 16:40:45 +0000

    Update config.py
    Added use of secret manager

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,15 +1,14 @@
-
-
 import os
+from app.security.secrets import get_secret
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    # Detect App Engine standard
+    if os.environ.get("GAE_ENV") == "standard":
+        MONGO_URI = get_secret("MONGO_URI")
+        SECRET_KEY = get_secret("FLASK_SECRET_KEY")
+    else:
+        MONGO_URI = os.environ.get("MONGO_URI")
+        SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    # MongoDB (Atlas)
-    MONGO_URI = os.environ.get("MONGO_URI")
-   
-    # Google Cloud Storage
-    GCS_BUCKET = os.getenv("GCS_BUCKET")
