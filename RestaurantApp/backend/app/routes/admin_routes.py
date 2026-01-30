fac683a9efd585bfaf1f17dfd4e42d345186b53b
mpalubicki <91080320+mpalubicki@users.noreply.github.com>
2026-01-30 23:06:35 +0000

    Delete RestaurantApp directory
    Remove duplicate

--- RestaurantApp/backend/app/routes/admin_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
-
-@admin_bp.route("/dashboard")
-def dashboard():
-    return "Admin dashboard coming soon"
--- RestaurantApp/backend/app/routes/menu_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from ..services.menu_service import get_menu_boxes
-
-menu_bp = Blueprint("menu", __name__)
-
-@menu_bp.route("/menu", methods=["GET", "POST"])
-def menu():
-    menu_by_category = get_menu_boxes(group_by_category=True)
-    return render_template("menu.html", menu_by_category=menu_by_category)
